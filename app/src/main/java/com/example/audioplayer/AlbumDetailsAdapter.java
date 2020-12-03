d4a5aabe96dbf1432874ace8441895205299a13f
Akira Rafhael Janson Pattirane <akira.rafhael@ti.ukdw.ac.id>
2020-12-03 09:49:42 +0700

    Add album details for songs

--- app/src/main/java/com/example/audioplayer/AlbumDetailsAdapter.java
+++ app/src/main/java/com/example/audioplayer/AlbumDetailsAdapter.java
@@ -1,7 +1,6 @@
 package com.example.audioplayer;
 
 import android.content.Context;
-import android.content.Intent;
 import android.media.MediaMetadataRetriever;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,11 +15,11 @@ import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
-public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.MyHolder> {
+public class AlbumDetailsAdapter extends RecyclerView.Adapter<AlbumDetailsAdapter.MyHolder> {
     private Context mContext;
     private ArrayList<MusicFiles> albumFiles;
     View view;
-    public AlbumAdapter(Context mContext, ArrayList<MusicFiles> albumFiles) {
+    public AlbumDetailsAdapter(Context mContext, ArrayList<MusicFiles> albumFiles) {
         this.mContext = mContext;
         this.albumFiles = albumFiles;
     }
@@ -28,13 +27,13 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.MyHolder> {
     @NonNull
     @Override
     public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        view = LayoutInflater.from(mContext).inflate(R.layout.album_item, parent, false);
+        view = LayoutInflater.from(mContext).inflate(R.layout.music_items, parent, false);
         return new MyHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyHolder holder, int position) {
-        holder.album_name.setText(albumFiles.get(position).getAlbum());
+        holder.album_name.setText(albumFiles.get(position).getTitle());
         byte[] image = getAlbumArt(albumFiles.get(position).getPath());
         if(image != null){
             Glide.with(mContext).asBitmap()
@@ -45,14 +44,6 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.MyHolder> {
                     .load(R.drawable.ic_music_note)
                     .into(holder.album_image);
         }
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(mContext, AlbumDetails.class);
-                intent.putExtra("albumName", albumFiles.get(position).getAlbum());
-                mContext.startActivity(intent);
-            }
-        });
     }
 
     @Override
@@ -63,13 +54,13 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.MyHolder> {
     public class MyHolder extends RecyclerView.ViewHolder{
         ImageView album_image;
         TextView album_name;
-    public MyHolder(View itemView)
-    {
-        super(itemView);
-        album_image = itemView.findViewById(R.id.album_image);
-        album_name = itemView.findViewById(R.id.album_name);
+        public MyHolder(View itemView)
+        {
+            super(itemView);
+            album_image = itemView.findViewById(R.id.music_img);
+            album_name = itemView.findViewById(R.id.file_name);
+        }
     }
-}
     private byte[] getAlbumArt(String uri){
         MediaMetadataRetriever retriever = new MediaMetadataRetriever();
         retriever.setDataSource(uri);
